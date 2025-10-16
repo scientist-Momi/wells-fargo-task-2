0b596bca25731cde4c6d0ab7b7bb4a83a15deba4
scientist-Momi <scientistmomi22@gmail.com>
2025-10-16 10:40:36 +0100

    Finishing Task Two

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -11,7 +11,7 @@ public class Portfolio {
     @Column(name = "portfolio_id")
     private long portfolioId;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "client_id", nullable = false)
     private Client client;
 
